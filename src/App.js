5a126b56a5e1f9e2bc20313376ddcb70a345c9ab
Luqman Butt <luqmanmbutt1@gmail.com>
2023-05-04 18:44:11 +0100

    added logic for item to add inside list

--- src/App.js
+++ src/App.js
@@ -8,20 +8,29 @@ function App() {
 
 
   const onTextChange = (event) => {
-    console.log("function is qworking", event.target.value)
+    
+    const item = event.target.value
+    setItemList(item)
+    console.log("itemlist", itemList)
   } 
 
   const submitValue = (event) => {
     event.preventDefault()
-    console.log("working submit")
+    console.log("working submit", event)
+    
   }
-  
   return (
     <div className="main-container">
       <form onSubmit={submitValue}>
         <input className="item-bar" type="text" onChange={onTextChange}></input>
         <button className="btn">Add</button>
       </form>
+      <ul>
+
+        <li>
+          {itemList}
+        </li>
+      </ul>
     </div>
   );
 }

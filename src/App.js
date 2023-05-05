42f25759902e9192dc706181650bfce8bda8bf58
Luqman Butt <luqmanmbutt1@gmail.com>
2023-05-05 19:29:20 +0100

    after typing what you want on your list you are able to add it to the list

--- src/App.js
+++ src/App.js
@@ -2,35 +2,37 @@ import './App.css';
 import React, { useState } from 'react'
 
 function App() {
-  
+
+  let itemResult = "" 
+
   const [itemList, setItemList] = useState([])
 
-
-
   const onTextChange = (event) => {
-    
-    const item = event.target.value
-    setItemList(item)
-    console.log("itemlist", itemList)
-  } 
+    itemResult = event.target.value
+    console.log("result",itemResult)
+  }
 
   const submitValue = (event) => {
     event.preventDefault()
-    console.log("working submit", event)
+    setItemList(itemResult)
     
   }
-  return (
-    <div className="main-container">
-      <form onSubmit={submitValue}>
-        <input className="item-bar" type="text" onChange={onTextChange}></input>
-        <button className="btn">Add</button>
-      </form>
-      <ul>
 
-        <li>
-          {itemList}
-        </li>
-      </ul>
+  return (
+    <div className='app-wrapper'>
+
+      <div className="main-container">
+        <form className="form-container"onSubmit={submitValue}>
+          <input className="item-bar" type="text" onChange={onTextChange}></input>
+          <button className="btn" >Add</button>
+        </form>
+        <ul>
+
+          <li>
+            {itemList}
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

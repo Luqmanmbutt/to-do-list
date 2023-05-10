35aca5b0cf205741424b27ed3243e58218c126d4
Luqman Butt <luqmanmbutt1@gmail.com>
2023-05-10 15:48:25 +0100

    item list works the item is being added

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 function App() {
 
-  let itemResult = ""
-
-  const itemList = []
-
+  
+  const [list, setList] = useState([])
+  const [itemList, setItemList] = useState("")
 
   const onTextChange = (event) => {
-    itemResult = event.target.value
+    setItemList(event.target.value)
   }
 
   const submitValue = (event) => {
     // event.preventDefault() stops it from refreshing the page 
     event.preventDefault()
-    itemList.push(itemResult)
+    if (itemList !== ""){
+      setList([list, itemList]);
+      setItemList('')
+    }
     event.target.reset();
   }
 
 
   return (
     <div className='app-wrapper'>
+      <div className='title'>To Do List </div>
       <div className="main-container">
         <form className="form-container" onSubmit={submitValue}>
-          <input className="item-bar" type="text" onChange={onTextChange}></input>
+          <input className="item-bar" type="text" value={itemList} onChange={onTextChange}></input>
           <button className="btn" >Add</button>
         </form>
-        <ul>
-          {itemList.map(info => {  
-            console.log("info", info)
+        <ul>      
+          {list.map(item => {  
             return (
             <>
-              <li>
-                {info}
+              <li >
+                {item}
+                
               </li>
             </>
             )
